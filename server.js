72ce5888a77d8156dffe82312813c61bed78ce9f
Sheikh Arman <sheikharman100@gmail.com>
2023-10-09 21:26:02 +0600

    backend starter

---
+++ server.js
@@ -0,0 +1,42 @@
+//all import
+const express=require("express")
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+
+
+//code 
+const app = express();
+
+//middleware for cookies
+app.use(cookieParser());
+// built-in middleware for json 
+app.use(express.json());
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// Cross Origin Resource Sharing
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"],//!write frontend route here
+  })
+);
+
+
+//routes
+app.use("/",(req,res)=>{
+    return res.send("The first route o LaundryMama")
+
+})
+
+
+
+
+//start the express server
+const PORT = process.env.PORT || 3501;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
