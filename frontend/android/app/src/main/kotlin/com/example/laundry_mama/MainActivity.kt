2c361f6b4cf0048274668cb87a82ef4d6b3aa6f3
saif64s <my@wo.rk>
2023-10-16 15:28:47 +0600

    frontend gone

--- frontend/android/app/src/main/kotlin/com/example/laundry_mama/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.laundry_mama
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend/lib/global/routes.dart
+++
@@ -1,5 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const String HOMEPAGE = '/';
-const String SIGNUP_PAGE = '/sign-up';
-const String LOGIN_PAGE = '/login';
