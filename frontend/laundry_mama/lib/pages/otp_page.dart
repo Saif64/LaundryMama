7925c146e5f399dfaeabb6c398bc04d1c47f43ab
saif64s <my@wo.rk>
2023-11-23 13:20:31 +0600

    OTP page background

--- frontend/laundry_mama/lib/pages/otp_page.dart
+++ frontend/laundry_mama/lib/pages/otp_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:laundry_mama/global/routes.dart';
 import 'package:lottie/lottie.dart';
 import 'package:otp_text_field/otp_text_field.dart';
@@ -16,62 +17,79 @@ class _OtpPageState extends State<OtpPage> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        elevation: 0,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Lottie.asset(
-            'assets/animations/otp.json',
-            height: height * 0.27,
-            frameRate: FrameRate.max,
+      extendBodyBehindAppBar: true,
+      body: Container(
+        height: height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xff688782), Color(0xff26e8e5)],
+            stops: [0.25, 0.75],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
           ),
-          SizedBox(
-            height: height * 0.03,
-          ),
-          const AutoSizeText(
-            'Enter your 6 digit OTP to authenticate your number',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          SizedBox(
-            height: height * 0.03,
-          ),
-          OTPTextField(
-            length: 6,
-            width: MediaQuery.of(context).size.width,
-            fieldStyle: FieldStyle.underline,
-            textFieldAlignment: MainAxisAlignment.spaceAround,
-            outlineBorderRadius: 8,
-            isDense: true,
-            onCompleted: (value) {
-              print("completed" + value);
-              Navigator.of(context).popUntil((route) => route.isFirst);
-              Navigator.pushReplacementNamed(context, HOMEPAGE);
-            },
-            style: const TextStyle(
-              fontSize: 17,
-            ),
-          ),
-          SizedBox(height: height * 0.02),
-          TextButton(
-              onPressed: () {},
-              child: const AutoSizeText(
-                'Send Again',
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Gap(height * 0.2),
+              Lottie.asset(
+                'assets/animations/otp.json',
+                height: height * 0.27,
+                reverse: true,
+                frameRate: FrameRate.max,
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              const AutoSizeText(
+                'Enter your 6 digit OTP to authenticate your number',
                 style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Colors.green,
+                  fontWeight: FontWeight.w600,
                 ),
-              ))
-        ],
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+                child: OTPTextField(
+                  length: 4,
+                  width: MediaQuery.of(context).size.width,
+                  fieldStyle: FieldStyle.box,
+                  textFieldAlignment: MainAxisAlignment.spaceAround,
+                  outlineBorderRadius: 10,
+                  fieldWidth: width * 0.15,
+                  contentPadding: const EdgeInsets.all(20),
+                  isDense: true,
+                  onCompleted: (value) {
+                    print("completed" + value);
+                    Navigator.of(context).popUntil((route) => route.isFirst);
+                    Navigator.pushReplacementNamed(context, HOMEPAGE);
+                  },
+                  style: const TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+              SizedBox(height: height * 0.02),
+              TextButton(
+                  onPressed: () {},
+                  child: const AutoSizeText(
+                    'Send Again',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
